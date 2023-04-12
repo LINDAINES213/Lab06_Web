ff6004ec49e652ea7f51bbfa731b6aa308ef3f32
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-04-11 20:49:36 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const images = [
+  {"src" : "/images/carlos.jpg"},
+  {"src" : "/images/checo.jpg"},
+  {"src" : "/images/hamilton.jpg"},
+  {"src" : "/images/lando.jpg"},
+  {"src" : "/images/leclerc.jpg"},
+  {"src" : "/images/ricciardo.jpg"},
+  {"src" : "/images/sargeant.jpg"},
+  {"src" : "/images/verstappen.jpg"}
+]
+
 function App() {
+
+  //shuffle cards
+
+  
+
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Memory Game</h1>
+      <button>New Game</button>
     </div>
   )
 }
