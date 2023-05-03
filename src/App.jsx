66c10c595a7d876af052159f683a75a226a2b2ca
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-05-02 19:47:17 -0600

    cambios finales

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 import MemoryCard from './components/MemoryCard'
 
 const imagenes = [
-  {"src" : "/images/carlos.jpg"},
-  {"src" : "/images/checo.jpg"},
-  {"src" : "/images/hamilton.jpg"},
-  {"src" : "/images/lando.jpg"},
-  {"src" : "/images/leclerc.jpg"},
-  {"src" : "/images/ricciardo.jpg"},
-  {"src" : "/images/sargeant.jpg"},
-  {"src" : "/images/verstappen.jpg"}
+  {"src" : "/images/carlos.jpeg", matched: false},
+  {"src" : "/images/checo.jpeg", matched: false},
+  {"src" : "/images/hamilton.jpeg", matched: false},
+  {"src" : "/images/lando.jpeg", matched: false},
+  {"src" : "/images/leclerc.jpeg", matched: false},
+  {"src" : "/images/ricciardo.jpeg", matched: false},
+  {"src" : "/images/sargeant.jpeg", matched: false},
+  {"src" : "/images/verstappen.jpeg", matched: false}
 ]
 
 function App() {
@@ -21,32 +19,78 @@ function App() {
   const [turns, setTurns] = useState(0)
   const[choiceOne, setChoiceOne] = useState(null)
   const[choiceTwo, setChoiceTwo] = useState(null)
+  const[disabled, setDisabled] = useState(false)
+  const [matchedPairs, setMatchedPairs] = useState(0)
 
-  //shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...imagenes, ...imagenes]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
 
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards)
     setTurns(0)
+    setMatchedPairs(0)
   }
 
-  //handle a choice
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
   }
 
+  useEffect(() => {
+    if(choiceOne && choiceTwo) {
+      setDisabled(true)
+      if(choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          const newCards = prevCards.map(card => {
+            if(card.src === choiceOne.src){
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          }) 
+          const matched = newCards.filter(card => card.matched).length
+          if(matched === 16) {
+            setMatchedPairs(matched)
+          }
+          return newCards
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
     <div className="App">
-      <h1>Memory Game</h1>
-      <button onClick={shuffleCards}>New Game</button>
+      <h1>Juego de Memoria F1</h1>
+      <h2>Turnos: {turns}</h2>
+      <button onClick={shuffleCards}>Nuevo Juego</button>
+      {matchedPairs === 16 && <h4>¡FELICITACIONES, GANASTE 🏆!</h4>}
       <div className="card-grid">
         {cards.map(card => (
           <MemoryCard 
           key={card.id} 
           card={card}
-          handleChoice={handleChoice}/> 
+          handleChoice={handleChoice}
+          flipped = {card === choiceOne || card === choiceTwo || card.matched}
+          disabled={disabled}
+      /> 
         ))}
       </div>
     </div>
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import './MemoryCard.css'
 
 
-function MemoryCard({ card, handleChoice }) {
+function MemoryCard({ card, handleChoice, flipped, disabled }) {
 
     const handleClick = () => {
+      if(!disabled){
         handleChoice(card)
+      }
+      
     }
 
     return (
         <div className="card">
-            <div>
-              <img className="front" src={card.src} alt="card front"/>
-              <img className="back" 
-              src="/images/formula1.png" 
-              onClick={handleClick} 
-              alt="card back"/>
+            <div className={flipped ? "flipped":""}>
+              <img className="front" src={card.src} alt="card front" height="180px" weight="50px"/>
+              <img className="back" src="/images/formula1.png" onClick={handleClick} alt="card back"/>
             </div>
           </div>
     )
